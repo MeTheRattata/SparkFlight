2c311c8cd133cf6d920e323b1f5e6ae8ae139462
Alisa Lytvyn <metherat@gmail.com>
2016-05-24 13:06:05 -0400

    Source charges spawn, they're all the same type though.

--- core/src/com/mygdx/sparkflight/SourceCharge.java
+++ core/src/com/mygdx/sparkflight/SourceCharge.java
@@ -8,7 +8,6 @@ public class SourceCharge extends Entities
 	
 	public SourceCharge(double x, double y, double c) 
 	{
-
 		super(x, y, c, c > 0 ? "positive" : "negative");
 		positionX = x;
 		positionY = y;
