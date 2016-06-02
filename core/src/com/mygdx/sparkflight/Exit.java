a7d3950bf19c96a0d57b851cf78bcf90a1a69ce2
Andrew Cater <Andrew Cater>
2016-06-02 12:08:02 -0400

    Added Exit Class

---
+++ core/src/com/mygdx/sparkflight/Exit.java
@@ -0,0 +1,20 @@
+package com.mygdx.sparkflight;
+
+import java.awt.Rectangle;
+
+public class Exit extends Entity 
+{
+	private Rectangle hitBox;
+	public Exit(float x, float y, double c, String name) 
+	{
+		super(x, y, c, name);
+		hitBox = new Rectangle(image.getWidth(),image.getWidth());
+	}
+	public void exitContainsPlayer (float x,float y)
+	{
+		if(hitBox.contains(x, y))
+		{
+			System.out.println("player has reached the exit!");
+		}
+	}
+}
