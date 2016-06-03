a86ed2238216846ba45b8824538805814c21bf86
Danny G <dgrossmann@gmail.com>
2016-06-03 11:42:53 -0400

    Added exit

--- core/src/com/mygdx/sparkflight/SparkFlight.java
+++ core/src/com/mygdx/sparkflight/SparkFlight.java
@@ -20,11 +20,13 @@ public class SparkFlight extends ApplicationAdapter {
 	private int width;
 	private int height;
 	public static AssetManager assets;
+	public Exit exit;
 	
 	@Override
 	public void create () 
 	{
 		assets = new AssetManager();
+		assets.load("exit.png", Texture.class);
 		assets.load("plane.png", Texture.class);
 		assets.load("positive.png", Texture.class);
 		assets.load("negative.png", Texture.class);
@@ -36,6 +38,7 @@ public class SparkFlight extends ApplicationAdapter {
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, width, height);
 		
+		exit = new Exit(450, 450, 0, "exit");
 		plane = new Player(150,150,0.0000000009,"plane");
 		
 		Gdx.input.setInputProcessor(new InputAdapter () {
@@ -64,6 +67,7 @@ public class SparkFlight extends ApplicationAdapter {
 		//Makes batch only show what is inside the camera's FOV
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
+		batch.draw(assets.get("exit.png", Texture.class), exit.getX(), exit.getY(), exit.getWidth(), exit.getHeight());
 		batch.draw(assets.get("plane.png", Texture.class), plane.getX(), plane.getY(), plane.getWidth(), plane.getHeight());
 //		System.out.println("Plane position x: " + plane.getMidPointX());
 //		System.out.println("Plane position y: " + plane.getMidPointY());
